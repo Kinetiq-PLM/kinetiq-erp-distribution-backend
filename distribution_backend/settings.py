069183b4ee6f4eef09118f6f07ccc8a67a6e9784
alexcszis <156319600+alexcszis@users.noreply.github.com>
2025-04-13 21:20:30 +0800

    added read me

--- distribution_backend/settings.py
+++ distribution_backend/settings.py
@@ -121,33 +121,33 @@ WSGI_APPLICATION = 'distribution_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Kinetiq-DB-SchemaV2',
-#         'USER': 'postgres',
-#         'PASSWORD': 'kinetiq',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         'OPTIONS': {
-#             'options': '-c search_path=distribution,public'
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'Kinetiq-DB'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'KntBg3jIY0DbpH8G9bwt'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '15432'),
+        'NAME': 'Kinetiq-DB-SchemaV2',
+        'USER': 'postgres',
+        'PASSWORD': 'kinetiq',
+        'HOST': 'localhost',
+        'PORT': '5432',
         'OPTIONS': {
             'options': '-c search_path=distribution,public'
         }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME', 'Kinetiq-DB'),
+#         'USER': os.getenv('DB_USER', 'postgres'),
+#         'PASSWORD': os.getenv('DB_PASSWORD', 'KntBg3jIY0DbpH8G9bwt'),
+#         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+#         'PORT': os.getenv('DB_PORT', '15432'),
+#         'OPTIONS': {
+#             'options': '-c search_path=distribution,public'
+#         }
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
